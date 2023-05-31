f3d5b5ea4d32f1cb71a3c13a7e229d13ab6dbf68
Ankit Pandey <113189919+pandeyankit123@users.noreply.github.com>
2023-05-31 19:31:40 +0530

    Updated Notepad to SmartTextEditor

--- GUI_SmartTextEditor.py
+++ GUI_SmartTextEditor.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.messagebox import showinfo
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+import re
 import os
 
 def newFile():
@@ -51,7 +52,65 @@ def copy():
 def paste():
     TextArea.event_generate(("<>"))
 def about():
-    showinfo("Notepad", "Notepad by Ankit Pandey")
+    showinfo("Notepad", "Notepad by Abdul,Ankit & Aryan")
+
+
+def find_text():
+    find_dialog = Toplevel(root)
+    find_dialog.title("Find")
+    find_dialog.transient(root)
+    
+    find_label = Label(find_dialog, text="Find:")
+    find_label.pack(side=LEFT)
+    find_entry = Entry(find_dialog)
+    find_entry.pack(side=LEFT, padx=5)
+    
+    find_button = Button(find_dialog, text="Find", command=lambda: find(find_entry.get()))
+    find_button.pack(side=LEFT)
+    
+    find_dialog.mainloop()
+
+def find(text):
+    start_pos = TextArea.search(text, "1.0", END)
+    if start_pos:
+        end_pos = start_pos + "+{}c".format(len(text))
+        TextArea.tag_remove("found", "1.0", END)
+        TextArea.tag_add("found", start_pos, end_pos)
+        TextArea.tag_config("found", background="yellow")
+        TextArea.mark_set("insert", start_pos)
+        TextArea.see("insert")
+    else:
+        showinfo("Not Found", "Text not found.")
+
+def replace_text():
+    replace_dialog = Toplevel(root)
+    replace_dialog.title("Replace")
+    replace_dialog.transient(root)
+    
+    find_label = Label(replace_dialog, text="Find:")
+    find_label.pack(side=LEFT)
+    find_entry = Entry(replace_dialog)
+    find_entry.pack(side=LEFT, padx=5)
+    
+    replace_label = Label(replace_dialog, text="Replace:")
+    replace_label.pack(side=LEFT)
+    replace_entry = Entry(replace_dialog)
+    replace_entry.pack(side=LEFT, padx=5)
+    
+    replace_button = Button(replace_dialog, text="Replace", command=lambda: replace(find_entry.get(), replace_entry.get()))
+    replace_button.pack(side=LEFT)
+    
+    replace_dialog.mainloop()
+
+def replace(text, replace_text):
+    content = TextArea.get("1.0", END)
+    modified_content = re.sub(text, replace_text, content)
+    if modified_content != content:
+        TextArea.delete("1.0", END)
+        TextArea.insert(END, modified_content)
+        showinfo("Success", "Text replaced successfully.")
+    else:
+        showinfo("Not Found", "Text not found.")
 
 if __name__ == '__main__':
     #Basic tkinter setup
@@ -90,6 +149,8 @@ if __name__ == '__main__':
     EditMenu.add_command(label = "Cut", command=cut)
     EditMenu.add_command(label = "Copy", command=copy)
     EditMenu.add_command(label = "Paste", command=paste)
+    EditMenu.add_command(label="Find", command=find_text)
+    EditMenu.add_command(label="Replace", command=replace_text)
 
     MenuBar.add_cascade(label="Edit", menu = EditMenu)
 
